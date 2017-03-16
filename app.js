ead15a846ab52e24beb6cedecd06e1ae2156b2c1
Diego Vinicius <diegowebd@hotmail.com>
2017-03-15 21:10:43 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+var notificacao = new Notificacao({titulo:'App Name', 
+								   body:'Mensagem Default', 
+								   icon:'path_do_icone', 
+								   status:1,
+								   action:'url_redirecionamento_ao_clicar'});
+notificacao.notificar();
